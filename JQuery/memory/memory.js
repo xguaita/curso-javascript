9c42dd56162209bd261da1b81d145d0727f3fd82
Xisco Guaita <xisco.guaita@gmail.com>
2014-11-06 18:48:13 +0100

    jquery memory

---
+++ JQuery/memory/memory.js
@@ -0,0 +1,94 @@
+var $cartaArriba = null, // Guarda la primera carta de una pareja de clicks
+  $cartas = null, // Contenedor (div) de la lista de cartas
+  $texto_panel = null, // Mensajes de texto
+  numClicks = 0, // Contador de clicks
+  parejas = 0, // Contador de parejas
+  multiplicando = 0,
+  multiplicador = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+
+
+function CrearCartas() {
+  var listaCartas = new Array(20);
+
+  // Crea la lista de multiplicaciones y resultados
+  for (var i = 0; i < 10; i++) {
+    listaCartas[i] = multiplicando + 'x' + multiplicador[i];
+    listaCartas[i + 10] = eval(multiplicando * multiplicador[i]);
+  }
+
+  // Mezcla las cartas
+  listaCartas = shuffle(listaCartas);
+
+  // Crea las cartas
+  for (var i = 0; i < 20; i++) {
+    $cartas.append('<div id=carta' + i + '><span>' + listaCartas[i] + '</span></div>');
+  }
+  // Añade un manejador del evento click a cada carta
+  $cartas.children("div").click(ClickCarta);
+}
+
+function EmpezarJuego() {
+  // Elimina cartas
+  $cartas.empty();
+
+  // Resetear texto del panel
+  $texto_panel.html("0 clicks");
+
+  // Inicializa variables
+  numClicks = 0;
+  parejas = 0;
+  multiplicando = Math.floor(Math.random() * 8) + 2; // Número aleatorio entre 2 y 9
+
+  // Crea cartas
+  CrearCartas();
+
+}
+
+function ClickCarta() {
+  var $carta = $(this),
+    $carta_span = $carta.find("span");
+
+  // No se puede hacer click en esta carta
+  $carta.unbind("click", ClickCarta);
+
+  // Contabiliza clicks
+  numClicks++;
+  $texto_panel.html(numClicks + ' clicks');
+
+  // Visualiza la carta
+  $carta_span.fadeToggle();
+
+  // Primera carta
+  if ($cartaArriba === null) {
+    $cartaArriba = $carta;
+  } else { // Segunda carta
+    // Son iguales?
+    if (eval($carta_span.text().replace('x', '*') + ' == ' + $cartaArriba.find("span").text().replace('x', '*'))) { // Sí
+      parejas++;
+    } else { // Diferentes
+      $cartaArriba.find("span").delay(600).fadeToggle();
+      $carta_span.delay(600).fadeToggle();
+      // Habilitar el click nuevamente
+      $cartaArriba.click(ClickCarta);
+      $carta.click(ClickCarta);
+    }
+    $cartaArriba = null;
+  }
+
+  // Hemos terminado?
+  if (parejas == 10) { // Todas las parejas
+    $texto_panel.html('Has encontrado todas las parejas con ' + numClicks + ' clicks');
+  } else if (numClicks == 40) { // Número máximo de intentos
+    $texto_panel.html('Has llegado al máximo de intentos y sólo has conseguido ' + parejas + ' pareja' + (parejas > 1 ? 's' : ''));
+    // Quita el manejador del evento click a todas las cartas
+    $cartas.children("div").unbind("click", ClickCarta);
+  }
+
+}
+
+$(function () {
+  $cartas = $('#cartas');
+  $texto_panel = $('#texto_panel');
+
+  EmpezarJuego();
+});
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-curso-javascript
+Curso JavaScript
 ================
 
-Introducción a la programación con javascript 
+Introducción a la programación con JavaScript
+
+Código fuente del curso
+
+## Miniproyectos
+
+### Juego memory (JQuery)
+Típico juego de cartas boca abajo donde hay que recordar la posición e ir haciendo parejas. En esta versión en lugar de imágenes se utilizan las tablas de multiplicar y se tienen como máximo 40 intentos para hacer las 10 parejas.
+
+<a href="http://rawgit.com/xguaita/curso-javascript/master/JQuery/memory/index.html" target="_blank">Jugar</a>
+
+
